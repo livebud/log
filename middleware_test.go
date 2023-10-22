fb068c3f4a6983b3c63d7d690ce2e4c6f02f66e6
Matthew Mueller <mattmuelle@gmail.com>
2023-10-22 14:35:51 -0500

    updates based on usage

--- middleware_test.go
+++ middleware_test.go
@@ -1,9 +1,11 @@
 package log_test
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/livebud/log"
 	"github.com/matryer/is"
@@ -18,6 +20,7 @@ func TestMiddleware(t *testing.T) {
 		log, err := log.FromContext(r.Context())
 		is.NoErr(err)
 		log.Field("cool", "story").Info("hello")
+		time.Sleep(100 * time.Millisecond)
 		w.Write([]byte("hello world"))
 	}))
 	req := httptest.NewRequest("GET", "http://livebud.com/docs", nil)
@@ -46,11 +49,12 @@ func TestMiddleware(t *testing.T) {
 	is.Equal(entries[2].Message, "response")
 	fields = entries[2].Fields
 	is.Equal(len(fields), 7)
-	is.Equal(fields.Keys()[0], "duration")
 	is.Equal(fields.Get("method"), "GET")
 	is.Equal(fields.Get("url"), "http://livebud.com/docs")
 	is.Equal(fields.Get("remote_addr"), "192.0.2.1:1234")
 	is.Equal(fields.Get("request_id"), requestId)
 	is.Equal(fields.Get("status"), 200)
 	is.Equal(fields.Get("size"), int64(11))
+	is.True(fields.Get("duration").(int64) > 0)
+	fmt.Println(fields.Get("duration"))
 }
